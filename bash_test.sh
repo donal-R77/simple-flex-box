78aa0b4e3791357601d28d9c05ae7bfb084da7ef
donal-R77 <rakotoarisoaromarick@gmail.com>
2024-07-26 18:44:34 +0300

    update

--- bash_test.sh
+++ bash_test.sh
@@ -5,7 +5,6 @@
 #IMAGE_NAME="maric77/flexbox:latest"
 
 # check if container exists or not
-if [[ $(docker ps -q -f name=flexdploy) ]] ; then
-    docker container stop flexdploy 
-fi
-    docker run -d -p 5000:5000 --name flexdeploy maric77/flexbox:latest
+if [[ $(docker ps -qqa -f name=flex) ]] ; then   docker container stop flex || \ { echo "Error faild to stop container" exit 1} fi && docker run -d -p 5000:5000 --name flex maric77/flexbox:flex
+
+
