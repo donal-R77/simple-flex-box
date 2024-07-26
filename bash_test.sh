2c7af2dd3fc7846951f36448c3e439653ee01c7e
donal-R77 <rakotoarisoaromarick@gmail.com>
2024-07-26 15:46:38 +0300

    remove the code

--- bash_test.sh
+++ bash_test.sh
@@ -2,12 +2,7 @@ PORTS="5000:5000"
 CONTAINER_NAME="flexdploy"
 IMAGE_NAME="maric77/flexbox:latest"
 
-if [[ $(docker run -d -p "$PORTS" --name flexdeploy maric77/flexbox:latest) ]]
-then 
-    docker container stop flexdploy
-fi 
-
-if [ "$(docker ps -qqa -f name=$CONTAINER_NAME)" ]
+if [[ "$(docker ps -qqa -f name=$CONTAINER_NAME)" ]]
  then
     docker container stop "$CONTAINER_NAME" && docker run -d -p "$PORTS" --name $CONTAINER_NAME $IMAGE_NAME
 else
