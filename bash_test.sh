c11530fc31ec698a5a5dbce148ec5ad518c39656
donal-R77 <rakotoarisoaromarick@gmail.com>
2024-07-26 16:02:00 +0300

    resolve an error

--- bash_test.sh
+++ bash_test.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
+
 PORTS="5000:5000"
 CONTAINER_NAME="flexdploy"
 IMAGE_NAME="maric77/flexbox:latest"
 
-if [[ "$(docker ps -qqa -f name=$CONTAINER_NAME)" ]]
- then
-    docker container stop "$CONTAINER_NAME" && docker run -d -p "$PORTS" --name $CONTAINER_NAME $IMAGE_NAME
+# check if container exists or not
+if [[ $(docker ps -q -f name=$CONTAINER_NAME) ]]; then
+    docker container stop "${CONTAINER_NAME}" && docker run -d -p "${PORTS}" --name "${CONTAINER_NAME}" "${IMAGE_NAME}"
 else
-    docker run -d -p "$PORTS" --name $CONTAINER_NAME $IMAGE_NAME
+    docker run -d -p "${PORTS}" --name "${CONTAINER_NAME}" "${IMAGE_NAME}"
+fi
