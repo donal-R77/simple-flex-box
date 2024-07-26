ef189b0a1d9e2098b662866d12229dd558163988
donal-R77 <rakotoarisoaromarick@gmail.com>
2024-07-26 17:34:57 +0300

    something deleted

--- bash_test.sh
+++ bash_test.sh
@@ -6,6 +6,6 @@
 
 # check if container exists or not
 if [[ $(docker ps -q -f name=flexdploy) ]] ; then
-    docker container stop flexdploy && docker run -d -p 5000:5000 --name flexdploy maric77/flexbox:latest
+    docker container stop flexdploy 
 fi
     docker run -d -p 5000:5000 --name flexdeploy maric77/flexbox:latest
