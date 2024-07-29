0bff92f4b2e764e3fea9236f8f495a1bf30f2394
donal-R77 <rakotoarisoaromarick@gmail.com>
2024-07-29 17:36:41 +0300

    script

--- bash_test.sh
+++ bash_test.sh
@@ -9,12 +9,17 @@ echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" foo --password-
 
 #Pull image in DockerHub
 docker pull "${IMAGE_NAME}"
-
+    
 # check if container exists or not
 if [[ $(docker ps -qqa -f name="${CONTAINER_NAME}") ]] ; then 
   docker container stop flex && docker container rm flex
 fi 
 docker run -d -p 5000:5000 --name flex maric77/flexbox:flex
-echo "$DOCKERNAME"
+
+#remove
+docker container prune -f 
+
+#image
+docker image prune -f
 
 
