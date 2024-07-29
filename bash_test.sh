cd5d50150ba030d9020fb9a83fa74ba781421f1b
donal-R77 <rakotoarisoaromarick@gmail.com>
2024-07-29 12:40:15 +0300

    use cloud storage$

--- bash_test.sh
+++ bash_test.sh
@@ -5,6 +5,9 @@
 #IMAGE_NAME="maric77/flexbox:latest"
 
 # check if container exists or not
-if [[ $(docker ps -qqa -f name=flex) ]] ; then   docker container stop flex || \ { echo "Error faild to stop container" exit 1} fi && docker run -d -p 5000:5000 --name flex maric77/flexbox:flex
+if [[ $(docker ps -qqa -f name=flex) ]] ; then 
+  docker container stop flex && docker container rm flex
+fi 
+docker run -d -p 5000:5000 --name flex maric77/flexbox:flex
 
 
