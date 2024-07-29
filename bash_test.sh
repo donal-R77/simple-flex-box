2da5ea0e1aff033611b1d1d26ba25ae73da2e9fb
donal-R77 <rakotoarisoaromarick@gmail.com>
2024-07-29 16:34:05 +0300

    Code refactoring

--- bash_test.sh
+++ bash_test.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-#PORTS="5000:5000"
-#CONTAINER_NAME="flexdploy"
-#IMAGE_NAME="maric77/flexbox:latest"
+PORTS="5000:5000"
+CONTAINER_NAME="flex"
+IMAGE_NAME="maric77/flexbox:flex"
+
+#login to Docker Hub
+echo "${DOCKER_PASSWORD}" | docker login -u ${DOCKER_USERNAME} foo --password-stdin
+
+#Pull image in DockerHub
+docker pull ${IMAGE_NAME}
 
 # check if container exists or not
-if [[ $(docker ps -qqa -f name=flex) ]] ; then 
+if [[ $(docker ps -qqa -f name=${CONTAINER_NAME}) ]] ; then 
   docker container stop flex && docker container rm flex
 fi 
 docker run -d -p 5000:5000 --name flex maric77/flexbox:flex
+echo "$DOCKERNAME"
 
 
