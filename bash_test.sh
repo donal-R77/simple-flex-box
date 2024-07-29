ccc442a14bf3b1fc3863aea1a8a3b2e7eb8fe5b1
donal-R77 <rakotoarisoaromarick@gmail.com>
2024-07-29 17:31:59 +0300

    Bash script

--- bash_test.sh
+++ bash_test.sh
@@ -5,13 +5,13 @@ CONTAINER_NAME="flex"
 IMAGE_NAME="maric77/flexbox:flex"
 
 #login to Docker Hub
-echo "${DOCKER_PASSWORD}" | docker login -u ${DOCKER_USERNAME} foo --password-stdin
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" foo --password-stdin
 
 #Pull image in DockerHub
-docker pull ${IMAGE_NAME}
+docker pull "${IMAGE_NAME}"
 
 # check if container exists or not
-if [[ $(docker ps -qqa -f name=${CONTAINER_NAME}) ]] ; then 
+if [[ $(docker ps -qqa -f name="${CONTAINER_NAME}") ]] ; then 
   docker container stop flex && docker container rm flex
 fi 
 docker run -d -p 5000:5000 --name flex maric77/flexbox:flex
