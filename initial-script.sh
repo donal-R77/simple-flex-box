e15ea928e32e0306271a907153678cc750233ebc
donal-R77 <rakotoarisoaromarick@gmail.com>
2024-09-17 10:43:57 +0300

    download s3

--- initial-script.sh
+++ initial-script.sh
@@ -6,9 +6,11 @@ sudo systemctl enable docker
 sudo chown root:docker /var/run/docker.sock
 sudo chmod a+w /var/run/docker.sock
 
-aws s3 cp s3://macdo-bucket/simple-app-flexbox/bash_test.sh .
+aws s3 cp s3://macdo-bucket/simple-app-flexbox/bash_test.sh /home/ec2-user/
 
-chmod a+x *.sh
+chmod +x /home/ec2-user/bash_test.sh
+
 export DOCKER_USERNAME='maric77'
 export DOCKER_PASSWORD='Groovy@22_05;'
-. bash_test.sh 
+
+/home/ec2-user/bash_test.sh
