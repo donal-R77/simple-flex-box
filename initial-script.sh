768a7473eaf7b5e8c93a16baf7455d3e4f0a6b89
donal-R77 <rakotoarisoaromarick@gmail.com>
2024-09-18 10:59:26 +0300

    change the script for ubuntu

--- initial-script.sh
+++ initial-script.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install docker -y
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo chown root:docker /var/run/docker.sock
-sudo chmod a+w /var/run/docker.sock
+sudo apt-get update -y
+sudo apt-get install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-aws configure set aws_access_key_id '${secrets.}'
+# Add the repository to Apt sources:
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+    sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+  sudo apt-get update -y
+
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo chown root:docker /var/run/docker.sock
+  sudo chmod a+w /var/run/docker.sock
+
 aws s3 cp s3://macdo-bucket/simple-app-flexbox/bash_test.sh /home/ec2-user/
 
 chmod +x /home/ec2-user/bash_test.sh
